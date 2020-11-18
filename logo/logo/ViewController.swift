b8ffabf23243de2f264f590329df00480967dda9
jeff kang <jeffkang@Jeffs-MBP.attlocal.net>
2020-11-18 01:03:51 -0600

    mvp complete

---
+++ logo/logo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  logo
+//
+//  Created by Jeff Kang on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoView: LogoView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
